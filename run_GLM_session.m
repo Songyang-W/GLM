60aa8527de84824b34e7f03db654c9640659df1a
Songyang-W <79537728+Songyang-W@users.noreply.github.com>
2022-01-19 17:09:27 -0500

    Update run_GLM_session.m

--- run_GLM_session.m
+++ run_GLM_session.m
@@ -1,21 +1,18 @@
 function run_GLM_session(directory, anm, session, plotfigure, ROI_list,Project,includeWhisker,denoise,plot_whiskers,coupling,enhsup)
 
 if nargin == 0
-    directory = 'Z:\Dropbox\Dropbox\Chen Lab Team Folder\Projects\Connectomics\Animals\';
-    anm = 'jc105';
-    session = 'jc105-19';
+    directory = 'Z:\Dropbox\Dropbox\Chen Lab Team Folder\Projects\CRACK\Animals\';
+    anm = 'cc032';
+    session = 'cc032-4';
     plotfigure = 1;
-%     ROI_list = {'jc105-19-A1-0145','jc105-19-A1-0109'};
-    ROI_list = {'jc105-19-A1-0145','jc105-19-A1-0109'};
-% ROI_list = {'jc105-18-A1-0059'};
-    Project = 'Connectomics';
-    includeWhisker = 0;
-    coupling = 0;
-    denoise = 1;
+    ROI_list = [];
+    Project = 'CRACK';
+    includeWhisker = 1;
+    coupling = 1;
+    denoise = 0;
     plot_whiskers = 0;
-    enhsup =  1;
+    enhsup =  0;
 end
-
 
 saved_day = convertStringsToChars(string(datetime('now','TimeZone','local','Format','_MMM_d_y_HH')));
 
